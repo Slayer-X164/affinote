a46c9c4598c6af628744f796721884599deeb3d1
Slayer-X164 <siddheshghag150@gmail.com>
2026-02-05 00:30:23 +0530

    added maintanance page

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,22 @@ import Footer from "@/components/ui/Footer";
 import Hero from "@/components/ui/Hero";
 import Navbar from "@/components/ui/Navbar";
 import TemplateSection from "@/components/ui/TemplateSection";
+import Image from "next/image";
 
 export default function Home() {
   return (
    <div className="min-h-screen ">
-    <AnnouncementBar/>
+    <div className="w-screen h-screen z-100  text-center gap-4 flex flex-col items-center justify-center bg-white position absolute left-0 top-0">
+      <Image src={"https://media.tenor.com/pPoUmi0Z1fUAAAAi/cat-pet.gif"} width={200} height={200} alt="maintance cat" ></Image>
+      <h1 className="text-3xl font-bold   text-blue-500">Affinote is Under Maintenance!!</h1>
+      <h3 className="animate-pulse">will be back soon...</h3>
+    </div>
+    {/* <AnnouncementBar/>
     <Navbar/>
     <Hero/>
     <TemplateSection/>
     <ContactBox/>
-    <Footer/>
+    <Footer/> */}
    </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.tenor.com",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        // Cache everything inside /public/images/*
+         source: "/:path*.(png|jpg|jpeg|gif|svg|webp|avif|ico|mp4|webm|woff|woff2|ttf|otf)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
