d93355098fd597f58e0d6dca5805d1542938a268
Slayer <siddheshghag150@gmail.com>
2026-02-05 19:04:07 +0530

    price change

--- data/mostUsed.ts
+++ data/mostUsed.ts
@@ -1,47 +1,68 @@
 export const MostUsedTemplates = [
+ {
+  id: "valentine_1",
+  title: "Valentine's Gift",
+  description:
+    "Beautiful Valentine Template with multiple gift options to choose from",
+  price: 99,
+  st_price: 149,
+  previewImg: "/previewImg/valentine_1.png",
+  fields: [
 
+    {
+      name: "your_message",
+      type: "textarea",
+      placeholder:
+        "In your eyes, I have found a home where my soul finally feels at rest. Every moment spent with you is a ...",
+    },
+
+  ],
+},
   {
     id: "flower-surprise",
     title: "Flower Surprise",
     description:
       "A cute X & O (Tic-Tac-Toe) template with a sweet surprise at the end",
-    price: 49,
-    st_price: 89,
+    price: 69,
+    st_price: 119,
     previewImg: "/previewImg/flowerSurp.png",
-    fields: [{ name: "text", type: "text" }],
+    fields: [
+      {
+        name: "start_message",
+        type: "text",
+        placeholder: "example: Open when u miss me !",
+      },
+      {
+        name: "end_message",
+        type: "textarea",
+        placeholder:
+          "example: Because you make every day as bright as a blooming flower.I miss you...",
+      },
+    ],
   },
   {
     id: "apology-for-gf",
     title: "Apology For GF/BF 2.0",
     description:
       "a small fun interactive Apology Template for your angry girlfriend",
-    price: 79,
-    st_price: 129,
+    price: 99,
+    st_price: 149,
     previewImg: "/previewImg/ApologyForGf.png",
-    fields: [{ name: "from", type: "text" }],
-  },
-  {
-    id: "memory-timeline",
-    title: "Memory Timeline",
-    description:
-      "A nostalgic memory timeline with photos and notes for your loved one",
-    price: 59,
-    st_price: 99,
-    previewImg: "/previewImg/memoTime.png",
     fields: [
-      { name: "title_1", type: "text" },
-
-      { name: "photo_1", type: "image" },
-      { name: "photo_2", type: "image" },
-
-      { name: "title_2", type: "text" },
-
-      { name: "photo_3", type: "image" },
-      { name: "photo_4", type: "image" },
-      { name: "photo_5", type: "image" },
-
-      { name: "end_message", type: "textarea" },
+      { name: "from_name", type: "text", placeholder: "example: Raj" },
+      {
+        name: "sorry_message",
+        type: "textarea",
+        placeholder:
+          "example: I’m really sorry, okay? I messed up and I know I hurt you. That was not cool of me at all...",
+      },
+      {
+        name: "end_message",
+        type: "text",
+        placeholder: "example: Got this Bouquet and a Cat for you baby❤️...",
+      },
     ],
   },
+
 
 ];
