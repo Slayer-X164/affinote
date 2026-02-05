5dc2135ee7c046b896dee4c1c9585daf64464f22
Slayer <siddheshghag150@gmail.com>
2026-02-05 18:01:23 +0530

    wokring commit

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
    <div className="min-h-screen ">
     <div className="w-screen h-screen z-100  text-center gap-4 flex flex-col items-center justify-center bg-white position absolute left-0 top-0">
-      <Image src={"https://media.tenor.com/pPoUmi0Z1fUAAAAi/cat-pet.gif"} width={200} height={200} alt="maintance cat" ></Image>
+      <Image src={"https://media.tenor.com/pPoUmi0Z1fUAAAAi/cat-pet.gif"} unoptimized width={200} height={200} alt="maintance cat" ></Image>
       <h1 className="text-3xl font-bold   text-blue-500">Affinote is Under Maintenance!!</h1>
       <h3 className="animate-pulse">will be back soon...</h3>
     </div>
