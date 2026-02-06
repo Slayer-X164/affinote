ccb7df0d10795e0b87e0da4bd5ebdd248a877779
Siddhesh Ghag <147908529+Slayer-X164@users.noreply.github.com>
2026-02-07 03:15:38 +0530

    Refactor next.config.ts for image patterns and heade

--- next.config.ts
+++ next.config.ts
@@ -3,32 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media.tenor.com",
-      },
-      {
-        protocol: "https",
-        hostname: "ik.imagekit.io",
-      },
+      { protocol: "https", hostname: "media.tenor.com" },
+      { protocol: "https", hostname: "ik.imagekit.io" },
     ],
   },
 
   async headers() {
     return [
       {
-        // Cache everything inside /public/images/*
-         source: "/:path*.(png|jpg|jpeg|gif|svg|webp|avif|ico|mp4|webm|woff|woff2|ttf|otf)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
+        source: "/",
+        headers: [{ key: "Cache-Control", value: "no-store" }],
+      },
+      {
+        source: "/:path*",
+        headers: [{ key: "Cache-Control", value: "no-store" }],
       },
     ];
   },
 };
 
 export default nextConfig;
-
